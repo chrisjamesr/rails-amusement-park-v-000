c27c0e112980ba5957d75dd9b338cc55e9f351ab
chrisjamesr <chrisjames.r@gmail.com>
2018-01-16 12:45:44 -0500

    build user  model with associations and required method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, :through => :rides
+  has_secure_password
+  # validates :password, :presence => true
+
+
+  def mood
+    if self.nausea > self.happiness
+      return "sad"
+    elsif self.happiness > self.nausea
+      return "happy"
+    end
+  end
+
 end
