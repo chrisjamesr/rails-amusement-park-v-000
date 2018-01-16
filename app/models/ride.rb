8729a35f5c1fdf6753afaafe9cac94393260f35d
chrisjamesr <chrisjames.r@gmail.com>
2018-01-16 12:45:27 -0500

    build ride  model with associations and required method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,21 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if user.height < attraction.min_height && user.tickets < attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}." 
+    else
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      binding.pry
+      user.save
+    end
+  end
+
 end
