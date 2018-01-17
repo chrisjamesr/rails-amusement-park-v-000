065c35857c26d052514926b7549811237b6b58ce
chrisjamesr <chrisjames.r@gmail.com>
2018-01-16 21:48:53 -0500

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
 
+  root 'attractions#index' 
+
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  get 'user/new' => 'users#new'
+  get 'users/new' => 'users#new'
   post 'users' => 'users#create'
   
 
