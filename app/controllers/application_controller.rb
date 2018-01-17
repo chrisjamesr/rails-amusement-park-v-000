5df5a535c76000169d58c8b6c0b37ff052a04122
chrisjamesr <chrisjames.r@gmail.com>
2018-01-16 21:48:35 -0500

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  private
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authentication_redirect
+    redirect_to login_path unless logged_in?
+  end
+
 end
