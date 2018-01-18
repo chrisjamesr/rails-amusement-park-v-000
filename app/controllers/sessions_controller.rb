c463212856fdf96070590c34de2fd5e3a32cdf4b
chrisjamesr <chrisjames.r@gmail.com>
2018-01-17 22:35:26 -0500

    build out session create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user =     
+    raise params.inspect
+    if @user = User.find_by(:name => params[:user][:name])    
+      if @user && user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
+    else
+      redirect_to new_user_path
+    end
   end
 
   def destroy
+    reset_session
+    flash[:notice] = "You have been logged out!"
+    render :new
 
   end
 end
