92a016805dc422d99987fb904e660237fba9d031
chrisjamesr <chrisjames.r@gmail.com>
2018-01-18 22:51:58 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
 
+  post '/rides' => 'rides#create'
+
   root 'welcome#home'
+  resources :attractions, :only => [:index, :show]
+  
 
-  get 'attractions/index' => 'attractions#index'
-
-  get '/login' => 'sessions#new'
+  get '/signin' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  resources :users, :only => [:new, :create, :show] 
+  resources :users, :only => [:new, :create, :show, :update] 
   
   
 
