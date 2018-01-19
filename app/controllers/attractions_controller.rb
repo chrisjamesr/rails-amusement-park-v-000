a5d8f8d8d32af7c9ef75ecb17768973e9560bc4d
chrisjamesr <chrisjames.r@gmail.com>
2018-01-18 22:52:41 -0500

    add before_action filter for current user

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,8 +1,13 @@
 class AttractionsController < ApplicationController
-  before_action :authentication_redirect, :only => [:index]
-  
+  before_action :authentication_redirect, :only => [:index, :show]
+  before_action :current_user, :only => [:show]
   def index
     @attractions = Attraction.all
+  end
+
+  def show
+
+    @attraction = Attraction.find(params[:id])
   end
 
 end
